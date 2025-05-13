c775620a1500abb037bfdea55800051f81f2dd8b
SamuelGlz10 <a01412958@tec.mx>
2025-05-12 20:30:01 -0600

    A ver que onda

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using std::cout;
 
 int main() {
-
+    /*Prueba*/
     std::cout << "sum(1, 5) = " << sum(1, 5) << '\n';
     std::cout << "sub(5, 1) = " << sub(5, 1) << '\n';
     std::cout << "div(9, 3) = " << divide(9, 3) << '\n';
